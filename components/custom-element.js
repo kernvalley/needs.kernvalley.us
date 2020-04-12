1f3aea48db548b82f994f0274875fb17f764e9cb
Chris Zuber <shgysk8zer0@gmail.com>
2020-04-12 14:17:34 -0700

    Wait for stylesheets before replacing components
    Also fixes `ready` handlers.

--- components/custom-element.js
+++ components/custom-element.js
@@ -1,9 +1,30 @@
 export default class HTMLCustomElement extends HTMLElement {
 	get ready() {
 		return new Promise(resolve => {
-			if (this.shadowRoot === null || this.shadowRoot.childElementCount === 0) {
-				this.addEventListener('ready', () => resolve(), {once: true});
+			if (this.shadowRoot !== null && this.shadowRoot.childElementCount === 0) {
+				this.addEventListener('ready', () => resolve(this), {once: true});
+			} else {
+				resolve(this);
 			}
+		}).then(() => this);
+	}
+
+	get stylesLoaded() {
+		return this.ready.then(async () => {
+			if (this.shadowRoot !== null) {
+				const stylesheets = this.shadowRoot.querySelectorAll('link[rel="stylesheet"][href]');
+				await Promise.all([...stylesheets].map(async link => {
+					if (! link.disabled && link.sheet === null) {
+						await new Promise((res, rej) => {
+							link.addEventListener('load', () => res(), {once: true});
+							link.addEventListener('error', () => rej(`Error loading ${link.href}`), {once: true});
+						});
+					}
+					// @TODO Wait for `@import` loading
+					// link.sheet.rules.filter(rule => rule.type === CSSRule.IMPORT_RULE)
+				}));
+			}
+			return this;
 		});
 	}
 
